3e7a2754f1ddc4944fa334253a7122cba9aebedb
mitanshu5911 <145962695+mitanshu5911@users.noreply.github.com>
2026-07-11 14:53:13 +0530

    Time: 7 ms (21.13%), Space: 8.8 MB (74.7%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.cpp
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -1,12 +1,7 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-        if(n<=0) return false;
-
-        while(n%3 == 0 ){
-            n=n/3;
-        }
-
-        return n==1;
+        // Powers of 3 must be positive, and must perfectly divide 3^19
+        return n > 0 && 1162261467 % n == 0;
     }
 };
