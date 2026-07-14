68a8666d4055f9a6193ffba2cdd52a3f9737692d
mitanshu5911 <145962695+mitanshu5911@users.noreply.github.com>
2026-07-14 09:18:33 +0530

    Time: 959 ms (55.65%), Space: 315.6 MB (26.09%) - LeetHub

---
+++ 3336-find-the-number-of-subsequences-with-equal-gcd/3336-find-the-number-of-subsequences-with-equal-gcd.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    static const int MOD = 1e9 + 7;
+
+    int subsequencePairCount(vector<int>& nums) {
+        const int MAX = 200;
+
+        vector<vector<int>> dp(MAX + 1, vector<int>(MAX + 1, 0));
+        vector<vector<int>> ndp(MAX + 1, vector<int>(MAX + 1, 0));
+
+        dp[0][0] = 1;
+
+        for (int x : nums) {
+
+            for (int i = 0; i <= MAX; i++)
+                fill(ndp[i].begin(), ndp[i].end(), 0);
+
+            for (int g1 = 0; g1 <= MAX; g1++) {
+                for (int g2 = 0; g2 <= MAX; g2++) {
+
+                    long long ways = dp[g1][g2];
+                    if (!ways) continue;
+
+                    // Skip
+                    ndp[g1][g2] = (ndp[g1][g2] + ways) % MOD;
+
+                    // Put into seq1
+                    int ng1 = (g1 == 0 ? x : gcd(g1, x));
+                    ndp[ng1][g2] = (ndp[ng1][g2] + ways) % MOD;
+
+                    // Put into seq2
+                    int ng2 = (g2 == 0 ? x : gcd(g2, x));
+                    ndp[g1][ng2] = (ndp[g1][ng2] + ways) % MOD;
+                }
+            }
+
+            dp.swap(ndp);
+        }
+
+        long long ans = 0;
+
+        for (int g = 1; g <= MAX; g++) {
+            ans = (ans + dp[g][g]) % MOD;
+        }
+
+        return ans;
+    }
+};
