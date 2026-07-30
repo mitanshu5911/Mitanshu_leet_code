ff246334494004a0a0eb2fecfd09ab942d1b5359
mitanshu5911 <145962695+mitanshu5911@users.noreply.github.com>
2026-07-30 13:43:52 +0530

    Time: 0 ms (100%), Space: 99.2 MB (70.04%) - LeetHub

--- 1791-find-center-of-star-graph/1791-find-center-of-star-graph.cpp
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.cpp
@@ -1,34 +1,10 @@
 class Solution {
 public:
     int findCenter(vector<vector<int>>& edges) {
-        unordered_map<int,set<int>> mpp;
-        for(int i=0; i<edges.size();i++){
-            int u=edges[i][0];
-            int v=edges[i][1];
 
-            mpp[u].insert(v);
-            mpp[v].insert(u);
-        }
-        int res = -1;
-        for(auto &it:mpp){
-            bool milgya = true;
-            for(auto &iz:mpp){
-                if(it.first!=iz.first){
-                    if( iz.second.count(it.first)){
-                        milgya=true;
-                    }else{
-                       milgya = false;
-                       break;
+        if(edges[0][0] == edges[1][0] || edges[0][0] == edges[1][1])
+            return edges[0][0];
 
-                    }
-                }
-            }
-
-            if(milgya){
-                res = it.first;
-            }
-        }
-
-        return res; 
+        return edges[0][1];
     }
 };
