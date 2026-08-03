91bfe4ffd302bb29cce43e9eb95e8f4a02e4fe1d
mitanshu5911 <145962695+mitanshu5911@users.noreply.github.com>
2026-08-03 11:29:29 +0530

    Time: 12 ms (86.94%), Space: 136.3 MB (65.5%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.cpp
+++ 1406-stone-game-iii/1406-stone-game-iii.cpp
@@ -2,24 +2,23 @@ class Solution {
 public:
     int n;
     vector<int> t;
-    int solve(vector<int>& stones, int i){
-        if(i>=n) return 0;
-        if(t[i]!=-1) return t[i];
-
-        int result = stones[i] - solve(stones,i+1);
-
-        if(i+1<n)
-            result = max(result,stones[i] + stones[i+1] - solve(stones,i+2));
-        if(i+2<n)
-            result = max(result, stones[i] + stones[i+1] + stones[i+2] - solve(stones,i+3));
-
-        return t[i] = result;
-    }
+    
     string stoneGameIII(vector<int>& arr) {
         n = arr.size();
-        t.resize(n+1,-1);
+        t.resize(n+1,0);
 
-        int diff = solve(arr,0);
+        for(int i=n-1; i>=0; i--){
+            t[i] = arr[i] - t[i+1];
+
+            if(i+1<n && i+2<=n)
+            t[i] = max(t[i], arr[i] + arr[i+1] - t[i+2]);
+
+            if(i+1<n && i+2<n && i+3<=n)
+            t[i] = max(t[i], arr[i] + arr[i+1] + arr[i+2] - t[i+3]);
+
+
+        }
+        int diff = t[0];
 
         if(diff < 0) return "Bob";
         else if(diff > 0) return "Alice";
